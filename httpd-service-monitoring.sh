6039cb395e1483fea3492bcf991195de18da8424
CloudFoundry276 <cloudfoundry276@gmail.com>
2024-03-05 18:59:34 +0000

    bash script for monitoring httpd service

---
+++ httpd-service-monitoring.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "######################################################################"
+date
+ls /var/run/httpd/httpd.pid > /dev/null
+
+if [ $? -eq 0 ]
+then
+    echo "HTTPD Process is Running"
+else
+    echo "HTTPD Process is Not Running"
+    echo "Starting the Process"
+    sudo systemctl start httpd
+
+    if [ $? -eq 0 ]
+    then
+        echo "HTTPD Process Started Successfully"
+    else
+        echo "HTTPD Process Starting Failed! Please Contact Admin"
+    fi
+fi
+echo "######################################################################"
+echo
+
